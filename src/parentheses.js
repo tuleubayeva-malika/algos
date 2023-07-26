a5cb20f5fbbb78f83bc3b690ac0739ba49a6917c
Malika Tuleubayeva <106465450+tuleubayeva-malika@users.noreply.github.com>
2023-07-26 18:26:03 +0600

    new changes

--- src/parentheses.js
+++ src/parentheses.js
@@ -1,4 +1,4 @@
-https://leetcode.com/problems/valid-parentheses/description/
+//https://leetcode.com/problems/valid-parentheses/description/
 
 /**
  * @param {string} s
--- src/reverse_words.js
+++ src/reverse_words.js
@@ -1,4 +1,4 @@
-/**
+// https://leetcode.com/problems/reverse-words-in-a-string/
  * @param {string} s
  * @return {string}
  * trim - ' ' 
